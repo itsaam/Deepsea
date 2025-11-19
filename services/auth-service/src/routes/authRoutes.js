c68cbd640d629fd9fb1ed606b3069c91f0a1ef84
Mitikx <t.san-juan@lprs.fr>
2025-11-19 16:09:26 +0100

    feat: add route for retrieving current user information

--- services/auth-service/src/routes/authRoutes.js
+++ services/auth-service/src/routes/authRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.get('/me', authMiddleware, authController.getme);
 router.post('/refresh', authMiddleware, authController.refreshToken);
 router.post('/verify', authController.verifyPassword);
 
